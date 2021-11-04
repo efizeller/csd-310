2e58fde5244db82a81f85de700eba568b33b7b3e
efizeller <75753654+efizeller@users.noreply.github.com>
2021-11-03 22:51:49 -0400

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -7,4 +7,7 @@ client = MongoClient(url)
 
 db = client.pytech
 
+print("\n -- Pytech COllection List --")
 print((db.list_collection_names))
+
+input("\n\n  End of program, press any key to exit... ")
